f9b9f2edc1fbaf25106437ce2e54ad6f13181765
xiaoxiaoqin <1192138940@qq.com>
2017-10-29 17:45:05 +0800

    11
    11

--- src/main/java/access/Lunch.java
+++ src/main/java/access/Lunch.java
@@ -1,38 +1,33 @@
 package access;
-
-//: access/Lunch.java
-// Demonstrates class access specifiers. Make a class
-// effectively private with private constructors:
-
 class Soup1 {
-  private Soup1() {}
-  // (1) Allow creation via static method:
-  public static Soup1 makeSoup() {
-    return new Soup1();
-  }
+	private Soup1() {
+	}
+	// (1) Allow creation via static method:
+	public static Soup1 makeSoup() {
+		//可以在返回引用之前做一些事情，比如计对象个数
+		return new Soup1();
+	}
 }
-
 class Soup2 {
-  private Soup2() {}
-  // (2) Create a static object and return a reference
-  // upon request.(The "Singleton" pattern):
-  private static Soup2 ps1 = new Soup2();
-  public static Soup2 access() {
-    return ps1;
-  }
-  public void f() {}
+	private Soup2() {
+	}
+	// (2) Create a static object and return a reference
+	// upon request.(The "Singleton" pattern):始终只能创建一个对象
+	private static Soup2 ps1 = new Soup2();
+	public static Soup2 access() {
+		return ps1;
+	}
+	public void f() {
+	}
 }
-
-// Only one public class allowed per file:
 public class Lunch {
-  void testPrivate() {
-    // Can't do this! Private constructor:
-    //! Soup1 soup = new Soup1();
-  }
-  void testStatic() {
-    Soup1 soup = Soup1.makeSoup();
-  }
-  void testSingleton() {
-    Soup2.access().f();
-  }
-} ///:~
+	void testPrivate() {		
+		// ! Soup1 soup = new Soup1();
+	}
+	void testStatic() {
+		Soup1 soup = Soup1.makeSoup();
+	}
+	void testSingleton() {
+		Soup2.access().f();
+	}
+} /// :~

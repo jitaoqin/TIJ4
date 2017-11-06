f6d9a8f673f089118057191203c1cccc4bb317cc
xiaoxiaoqin <1192138940@qq.com>
2017-11-06 19:28:08 +0800

    20171106 家
    20171106 家

--- src/main/java/polymorphism/Sandwich.java
+++ src/main/java/polymorphism/Sandwich.java
@@ -1,46 +1,63 @@
 //: polymorphism/Sandwich.java
 // Order of constructor calls.
 package polymorphism;
+
 import static net.mindview.util.Print.*;
 
+/*
+ * 构造器检查对象是否被正确地构造。导出类只能访问它自己的成员，不能访问基类中的成员。
+ * 只有基类的构造器才具有恰当的知识和权限来对自己的元素进行初始化。因此必须令所有构造器都得到调用，否则
+ * 不能正确构造完整对象。
+ * 
+ */
 class Meal {
-  Meal() { print("Meal()"); }
+	Meal() {
+		print("Meal()");
+	}
 }
 
 class Bread {
-  Bread() { print("Bread()"); }
+	Bread() {
+		print("Bread()");
+	}
 }
 
 class Cheese {
-  Cheese() { print("Cheese()"); }
+	Cheese() {
+		print("Cheese()");
+	}
 }
 
 class Lettuce {
-  Lettuce() { print("Lettuce()"); }
+	Lettuce() {
+		print("Lettuce()");
+	}
 }
 
 class Lunch extends Meal {
-  Lunch() { print("Lunch()"); }
+	Lunch() {
+		print("Lunch()");
+	}
 }
 
 class PortableLunch extends Lunch {
-  PortableLunch() { print("PortableLunch()");}
+	PortableLunch() {
+		print("PortableLunch()");
+	}
 }
 
 public class Sandwich extends PortableLunch {
-  private Bread b = new Bread();
-  private Cheese c = new Cheese();
-  private Lettuce l = new Lettuce();
-  public Sandwich() { print("Sandwich()"); }
-  public static void main(String[] args) {
-    new Sandwich();
-  }
-} /* Output:
-Meal()
-Lunch()
-PortableLunch()
-Bread()
-Cheese()
-Lettuce()
-Sandwich()
-*///:~
+	private Bread b = new Bread();
+	private Cheese c = new Cheese();
+	private Lettuce l = new Lettuce();
+
+	public Sandwich() {
+		print("Sandwich()");
+	}
+
+	public static void main(String[] args) {
+		new Sandwich();
+	}
+} /*
+	 * Output: Meal() Lunch() PortableLunch() Bread() Cheese() Lettuce() Sandwich()
+	 */// :~
--- src/main/java/polymorphism/shape/RandomShapeGenerator.java
+++ src/main/java/polymorphism/shape/RandomShapeGenerator.java
@@ -2,7 +2,10 @@
 // A "factory" that randomly creates shapes.
 package polymorphism.shape;
 import java.util.*;
-
+/*
+ * 将一个方法调用同一个方法主体关联起来称为绑定，若程序执行前进行绑定成为前期绑定，
+ * 后期绑定或动态绑定
+ */
 public class RandomShapeGenerator {
   private Random rand = new Random(47);
   public Shape next() {
